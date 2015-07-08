f12071bc87d7cd0ad27fd1d10bcc113334392bfb
prudvihemanth <prudvihemanth@gmail.com>
2015-07-08 18:23:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,107 +1,31 @@
-var Hapi = require('hapi');
-var mongoose = require('mongoose');
-var server = new Hapi.Server();
-mongoose.connect('mongodb://localhost:27017/angular-hapi-db');
+var exampleModule = angular.module('ExampleApp', []);
 
-var Schema = mongoose.Schema;
+exampleModule.controller('UserController', function($scope, $http){
+	$scope.myMessage = function(){
+		return "Hi prudvi, cool man!";
+	};
 
-var UserSchema = new Schema({fname: {type: String, unique: true, index: true}, lname: String, lname3: String, lname4: Number, button1: String});
+	$scope.createUser = function(){
+		var fName = $scope.fname;
+		var lName = $scope.lname;
+		var lName3 = $scope.lname3;
+		var lName4 = $scope.lname4;
+		var button1 = $scope.button1;
 
-var User = mongoose.model('User', UserSchema);
+		console.log(fName);
+		console.log(lName);
+		console.log(lName3);
+		console.log(lName4);
+		console.log(button1);
 
-server.connection({ port: 3000 });
+		var rootUrl = "http://localhost:3000";
 
-server.ext('onRequest', function(request, reply){
-  console.log('Request recived: '+ request.path);
-  reply.continue();
-})
+		var url = rootUrl + "/user?fname=" + fName + "&lname=" + lName + "&lname3=" + lName3 + "&lname4=" + lName4 + "&button1=" + button1 ;
 
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: 'public',
-      listing: true
-    }
-  }
-});
-
-
-server.route({
-    method: 'POST',
-    path: '/',
-    handler: function (request, reply) {
-        reply('I did something!');
-    }
-});
-
-
-
-server.route({
-	path: '/user',
-	method: 'GET',
-	handler: createUser
-})
-
-/*
-function createUser(request, reply){
-	console.log(request.query);
-  fname = request.query.fname;
-  lname = request.query.lname;
-  lname3 = request.query.lname3;
-  lname4 = request.query.lname4;
-  button1 = request.query.button1;
-  User.find({fname: fname}, function(err, users){
-    console.log(users);
-    if(users.length == 0){
-      return User.create({fname: fname, lname: lname, lname3: lname3, lname4: lname4, button1: button1}, function(err, dbUser){
-        reply({message: "User created"});
-      });
-    }
-    user = users[0];
-    user.lname = lname;
-    user.save(function(err){
-      reply({message: "User updated"});
-    });
-  });
-}
-
-*/
-
-function createUser(request, reply){
-  var fname = request.query.fname;
-  var lname = request.query.lname;
-  var lname3 = request.query.lname3;
-  var lname4 = request.query.lname4;
-  var button1 = request.query.button1;
-  console.log("in createUser function");
-  var mongoQuery = {
-    $and: [
-      {fname: fname},
-      {lname: lname},
-      {lname3: lname3}
-    ] 
-  };
-
-  User.find(mongoQuery, function(err, users){
-    console.log(users);
-    if(users.length == 0){
-      return User.create({fname: fname, lname: lname, lname3: lname3, lname4: lname4, button1: button1}, function(err, dbUser){
-      });
-    }
-    console.log("hi i can find same user");
-    user = users[0];
-    user.lname4 = lname4;
-    user.save(function(err){
-      reply({message: "User updated"});
-    });
-  });
-}
-
-
-
-
-server.start(function(){
-  console.log("Serer running on port: " + server.info.port);
+		console.log(url);
+		$http.get(url).success(function(data, status, headers, config){
+			console.log(data); 
+		})
+	};
+	
 });
